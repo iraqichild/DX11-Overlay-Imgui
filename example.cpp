1a138f2e90d039a235f019caf4ced0f8c24bcc60
iraqichild <heapallocation@proton.me>
2024-12-24 00:47:54 -0700

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,28 @@
+#include "Lib/Overlay/SimpleOv.hpp"
+
+int main()
+{
+    overlay::SetupWindow();
+    if (!(overlay::CreateDeviceD3D(overlay::Window)))
+        return 1;
+
+    while (!overlay::ShouldQuit)
+    {
+        overlay::Render();
+
+        ImGuiIO& io = ImGui::GetIO();
+        float fps = 1.0f / io.DeltaTime;
+
+        ImGui::Begin("ImGui");
+
+        ImGui::Text("FPS: %.2f", fps);
+            
+        ImGui::End();
+
+        ImGui::GetForegroundDrawList()->AddRectFilled(ImVec2(50, 150), ImVec2(150, 50), ImColor(255, 255, 255, 255));
+      
+        overlay::EndRender();
+    }
+
+    overlay::CloseOverlay();
+}
